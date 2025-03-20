a587fb91e8ca63ae6bdc4c55567660adeb34c899
Yuki Takahara <134689144+yuki10000@users.noreply.github.com>
2025-03-20 15:41:47 +0900

    Revert "#532 忍者Admaxの設定を追加"

--- resources/views/WelcomeView.vue
+++ resources/views/WelcomeView.vue
@@ -1,28 +1,16 @@
 <script setup>
-  import { onMounted } from "vue";
   import Welcome from "../components/Welcome.vue";
   import SearchClasses from "../components/SearchClasses.vue";
   import FontChangeTitle from "../components/shared/FontChangeTitle.vue";
-
-  onMounted(() => {
-    const container = document.getElementById("admax-container"); // 追加するターゲットの要素
-    if (container) {
-      // 1. `script` タグを作成
-      const script = document.createElement("script");
-      script.src = "https://adm.shinobi.jp/s/a275cef6960a6f78931b7f2f1dae41c4"; // 忍者AdmaxのスクリプトURL
-      container.appendChild(script);
-    }
-  });
 </script>
 
 <template>
   <v-container class="pb-8 bg-primary" fluid>
-    <FontChangeTitle> 大学生による授業評価を、オープンに。 </FontChangeTitle>
+    <FontChangeTitle>
+      大学生による授業評価を、オープンに。
+      <!-- ~広大生の学びを支える情報サイト~ -->
+    </FontChangeTitle>
   </v-container>
   <SearchClasses></SearchClasses>
-
-  <!-- admax 広告の表示エリア -->
-  <div id="admax-container"></div>
-
   <Welcome></Welcome>
 </template>
